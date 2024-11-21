686922a9367e44b6e7f9f3dac90b70db625421d7
chanmin0311 <ckstj1500@gmail.com>
2024-11-21 19:28:28 +0900

    초기 커밋.

---
+++ MainForm/MainForm/Form2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UserInfo;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserRegistration user = new UserRegistration("userInfo.txt", "userScore.txt");
+            if (user.RegisterUser
+                (
+                    emailTxtBox,
+                    phoneTxtBox,
+                    nameTxtBox,
+                    idTxtBox,
+                    pwTxtBox,
+                    pwcheckTxtBox
+                ))
+            {
+                this.Close();
+            }
+            
+        }
+
+        private  void emailTxtBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (emailTxtBox.Text.Equals("입력해 주세요."))
+            {
+                pwcheckTxtBox.ForeColor = System.Drawing.Color.DarkRed;
+                emailTxtBox.Text = "";
+            }
+        }
+
+        private void phoneTxtBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            phoneTxtBox.ForeColor= System.Drawing.Color.DarkRed;
+            if (phoneTxtBox.Text.Equals("입력해 주세요."))
+            {
+                pwcheckTxtBox.ForeColor = System.Drawing.Color.DarkRed;
+                phoneTxtBox.Text = "";
+            }
+        }
+
+        private void nameTxtBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (nameTxtBox.Text.Equals("입력해 주세요."))
+            {
+                nameTxtBox.Text = "";
+            }
+        }
+
+        private void idTxtBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (idTxtBox.Text.Equals("입력해 주세요."))
+            {
+                idTxtBox.Text = "";
+            }
+        }
+
+        private void pwTxtBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (pwTxtBox.Text.Equals("입력해 주세요."))
+            {
+                pwTxtBox.Text = "";
+            }
+
+        }
+
+        private void pwcheckTxtBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            pwcheckTxtBox.ForeColor = System.Drawing.Color.DarkRed;
+            if (pwcheckTxtBox.Text.Equals("입력해 주세요."))
+            {
+                pwcheckTxtBox.Text = "";
+            }
+        }
+    }
+}
